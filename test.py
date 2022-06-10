9df744d0718538adf067e278c1d58022f3e2537e
Eugene Baraka <eugenebaraka@gmail.com>
2022-06-09 20:52:21 -0400

    removed confidential info

--- test.py
+++ test.py
@@ -16,7 +16,32 @@ with open("msg.txt", encoding= 'utf8') as f:
 
 for contact in contacts:
     search_xpath = '//div[@contenteditable="true"][@data-tab="3"]'
-    search_box = WebDriverWait(browser, 500).until(EC.presence_of_element_located((By.XPATH, search_xpath)))
-    searchbox.send_keys(contact)
+    search_box = WebDriverWait(browser, 500).until(
+        EC.presence_of_element_located((By.XPATH, search_xpath)))
+
+    search_box.clear()
+
+
+    time.sleep(5)
+
+    pyperclip.copy(contact)
+    search_box.send_keys(Keys.CONTROL, 'v')
+
+    time.sleep(5)
+
+    send_xpath = f'//span[@title="{contact}"]'
+    person_name = browser.find_element(By.XPATH, send_xpath)
+    person_name.click()
+    message_box_xpath = '//div[@contenteditable="true"][@data-tab="10"]'
+    message_input = browser.find_element_by_xpath(message_box_xpath)
+
+    time.sleep(5)
+
+    pyperclip.copy(message)
+    message_input.send_keys(Keys.CONTROL, 'v')
+    message_input.send_keys(Keys.ENTER)
+
+
+    time.sleep(5)
 
 
